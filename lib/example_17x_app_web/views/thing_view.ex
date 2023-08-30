998451319fd032017848dc6ff3ae47e9ca30fb88
Declan McGrath <declan@weuseopensource.com>
2023-08-30 21:09:15 +0100

    Satisfying the system's desire for a "Example17xAppWeb.ThingJSON-API" view

--- lib/example_17x_app_web/views/thing_view.ex
+++ lib/example_17x_app_web/views/thing_view.ex
@@ -1,5 +1,10 @@
-defmodule Example17xAppWeb.ThingView do
-  use Example17xAppWeb, :view
+# Using an erlang atom as module name, otherwise dashes would not be allowed in the module name.
+# And if I don't do this then I get the error:
+#
+#   (ArgumentError) no "show" json-api template defined for
+#   :"Elixir.Example17xAppWeb.ThingJSON-API"  (the module does not exist)
+#
+defmodule :"Elixir.Example17xAppWeb.ThingJSON-API" do
   use JaSerializer.PhoenixView
 
   attributes([:volume_level])
