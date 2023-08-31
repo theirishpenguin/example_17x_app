4dc5949f34ea7007273679fe7b105527a651501a
Declan McGrath <declan@weuseopensource.com>
2023-08-31 10:52:02 +0100

    Using put_view() to clean up the view module naming.

--- lib/example_17x_app_web/views/thing_view.ex
+++ lib/example_17x_app_web/views/thing_view.ex
@@ -1,19 +1,9 @@
-# Using an erlang atom as module name, otherwise dashes would not be allowed in the module name.
-# And if I don't do this then I get the error:
-#
-#   (ArgumentError) no "show" json-api template defined for
-#   :"Elixir.Example17xAppWeb.ThingJSON-API"  (the module does not exist)
-#
-defmodule :"Elixir.Example17xAppWeb.ThingJSON-API" do
+defmodule Example17xAppWeb.ThingView do
   use JaSerializer.PhoenixView
 
   attributes([:volume_level])
 
   def id(_data, _conn) do
     "singular"
-  end
-
-  def type do
-    "things"
   end
 end
